309855f7b8f421c75f3cdcfa127603ff61bd8e67
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-03-04 13:31:00 -0600

    First commit

---
+++ src/components/Usuario.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+interface User {
+  uid: string;
+  name: string;
+}
+
+const Usuario = () => {
+  const [user, setUser] = useState<User>();
+
+  const login = () => {
+    setUser({
+      uid: "ABC",
+      name: "Fernando herrera",
+    });
+  };
+
+  return (
+    <div className="mt-5">
+      <h3>Usuario: useState</h3>
+      <button onClick={login} className="btn btn-outline-primary">
+        Login
+      </button>
+
+      {!user ? <pre>No hay usuario</pre> : <pre>{JSON.stringify(user)}</pre>}
+    </div>
+  );
+};
+
+export default Usuario;
